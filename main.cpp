f5e325568ad361320a99b6a718bc5cc15791d5e9
Koyshem <koyshemfoley@yahoo.com>
2023-06-01 01:33:08 +0300

    V3

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <chrono>
 #include <thread>
 
-const int COMPLEXITY = 280;
+const int COMPLEXITY = 200;
 int WINDOW_WIDTH = COMPLEXITY;
 int WINDOW_HEIGHT = COMPLEXITY;
 const int CELL_SIZE = 40;
@@ -22,6 +22,27 @@ struct Cell {
     CellState state = CellState::Hidden;
     bool hasMine = false;
 };
+
+void resetField(std::vector<std::vector<Cell>>& field) {
+    for (int x = 0; x < FIELD_WIDTH; x++) {
+        for (int y = 0; y < FIELD_HEIGHT; y++) {
+            field[x][y].state = CellState::Hidden;
+            field[x][y].hasMine = false;
+        }
+    }
+}
+
+void initializeField(std::vector<std::vector<Cell>>& field, int mineCount) {
+    srand(time(NULL));
+    while (mineCount > 0) {
+        int x = rand() % FIELD_WIDTH;
+        int y = rand() % FIELD_HEIGHT;
+        if (!field[x][y].hasMine) {
+            field[x][y].hasMine = true;
+            mineCount--;
+        }
+    }
+}
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Minefield");
@@ -50,6 +71,12 @@ int main() {
         return 1;
     }
 
+    sf::Font font;
+    if (!font.loadFromFile("Pixeled.ttf")) {
+        std::cout << "Error Pixeled.ttf" << std::endl;
+        return 1;
+    }
+
     sf::Sprite hiddenSprite(hiddenTexture);
     sf::Sprite revealedSprite(revealedTexture);
     sf::Sprite mineSprite(mineTexture);
@@ -57,186 +84,221 @@ int main() {
 
     std::vector<std::vector<Cell>> field(FIELD_WIDTH, std::vector<Cell>(FIELD_HEIGHT));
 
-    srand(time(NULL));
-    int mineCount = MINES;
-    while (mineCount > 0) {
-        int x = rand() % FIELD_WIDTH;
-        int y = rand() % FIELD_HEIGHT;
-        if (!field[x][y].hasMine) {
-            field[x][y].hasMine = true;
-            mineCount--;
+    bool running = true;
+    bool restartRequested = false;
+
+    while (running) {
+        if (restartRequested) {
+            resetField(field);
+            initializeField(field, MINES);
         }
-    }
 
-    int startX = 0; // Начальная позиция игрока
-    int startY = 0;
-    int endX = FIELD_WIDTH - 1; // Конечная позиция
-    int endY = FIELD_HEIGHT - 1;
+        // Menu
+        sf::Text startText;
+        startText.setFont(font);
+        startText.setCharacterSize(24);
+        startText.setString("Start");
+        startText.setFillColor(sf::Color::White);
+        startText.setPosition(WINDOW_WIDTH / 2 - startText.getLocalBounds().width / 2,
+                              WINDOW_HEIGHT / 2 - startText.getLocalBounds().height);
 
-    bool gameOver = false;
-    bool win = false;
+        sf::Text restartText;
+        restartText.setFont(font);
+        restartText.setCharacterSize(24);
+        restartText.setString("Restart");
+        restartText.setFillColor(sf::Color::White);
+        restartText.setPosition(WINDOW_WIDTH / 2 - restartText.getLocalBounds().width / 2,
+                                WINDOW_HEIGHT / 2 + restartText.getLocalBounds().height);
 
-    // Пауза перед началом игры для показа мин
-    for (int x = 0; x < FIELD_WIDTH; x++) {
-        for (int y = 0; y < FIELD_HEIGHT; y++) {
-            if (field[x][y].hasMine) {
-                field[x][y].state = CellState::Mine;
-            }
-        }
-    }
+        window.clear();
+        window.draw(startText);
+        window.display();
 
-    // Отображение мин на паузе
-    window.clear();
-    for (int x = 0; x < FIELD_WIDTH; x++) {
-        for (int y = 0; y < FIELD_HEIGHT; y++) {
-            int posX = x * CELL_SIZE;
-            int posY = y * CELL_SIZE;
-
-            if (field[x][y].state == CellState::Mine) {
-                mineSprite.setPosition(posX, posY);
-                window.draw(mineSprite);
-            }
-        }
-    }
-    window.display();
-
-    // Пауза на 5 секунд
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-
-    // Сброс состояния поля
-    for (int x = 0; x < FIELD_WIDTH; x++) {
-        for (int y = 0; y < FIELD_HEIGHT; y++) {
-            if (field[x][y].hasMine) {
-                field[x][y].state = CellState::Hidden;
-            }
-        }
-    }
-
-    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-            else if (event.type == sf::Event::MouseButtonReleased) {
-                if (!gameOver && !win) {
-                    if (event.mouseButton.button == sf::Mouse::Left) {
-                        int mouseX = event.mouseButton.x / CELL_SIZE;
-                        int mouseY = event.mouseButton.y / CELL_SIZE;
+        while (window.waitEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                running = false;
+                break;
+            } else if (event.type == sf::Event::MouseButtonReleased) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    int mouseX = event.mouseButton.x;
+                    int mouseY = event.mouseButton.y;
 
-                        if (mouseX >= 0 && mouseX < FIELD_WIDTH && mouseY >= 0 && mouseY < FIELD_HEIGHT) {
-                            if (field[mouseX][mouseY].state == CellState::Hidden) {
-                                if (field[mouseX][mouseY].hasMine) {
-                                    gameOver = true;
-                                    std::cout << "You lost!" << std::endl;
-                                } else {
-                                    field[mouseX][mouseY].state = CellState::Revealed;
-                                    std::cout << "Opened!" << std::endl;
+                    if (startText.getGlobalBounds().contains(mouseX, mouseY) || restartRequested) {
+                        restartRequested = false;
+
+                        resetField(field);
+                        initializeField(field, MINES);
+
+                        bool gameOver = false;
+                        bool win = false;
+
+                        // Пауза перед началом игры для показа мин
+                        for (int x = 0; x < FIELD_WIDTH; x++) {
+                            for (int y = 0; y < FIELD_HEIGHT; y++) {
+                                if (field[x][y].hasMine) {
+                                    field[x][y].state = CellState::Mine;
                                 }
                             }
                         }
-                    }
 
-                    // Проверка на победу
-                    bool allNonMinesRevealed = true;
-                    for (int x = 0; x < FIELD_WIDTH; x++) {
-                        for (int y = 0; y < FIELD_HEIGHT; y++) {
-                            if (!field[x][y].hasMine && field[x][y].state != CellState::Revealed) {
-                                allNonMinesRevealed = false;
+                        // Отображение мин на паузе
+                        window.clear();
+                        for (int x = 0; x < FIELD_WIDTH; x++) {
+                            for (int y = 0; y < FIELD_HEIGHT; y++) {
+                                int posX = x * CELL_SIZE;
+                                int posY = y * CELL_SIZE;
+
+                                if (field[x][y].state == CellState::Mine) {
+                                    mineSprite.setPosition(posX, posY);
+                                    window.draw(mineSprite);
+                                }
+                            }
+                        }
+                        window.display();
+
+                        // Пауза на 5 секунд
+                        std::this_thread::sleep_for(std::chrono::seconds(5));
+
+                        // Сброс состояния поля
+                        for (int x = 0; x < FIELD_WIDTH; x++) {
+                            for (int y = 0; y < FIELD_HEIGHT; y++) {
+                                if (field[x][y].hasMine) {
+                                    field[x][y].state = CellState::Hidden;
+                                }
+                            }
+                        }
+
+                        while (window.isOpen()) {
+                            while (window.pollEvent(event)) {
+                                if (event.type == sf::Event::Closed) {
+                                    window.close();
+                                } else if (event.type == sf::Event::MouseButtonReleased) {
+                                    if (!gameOver && !win) {
+                                        if (event.mouseButton.button == sf::Mouse::Left) {
+                                            int mouseX = event.mouseButton.x / CELL_SIZE;
+                                            int mouseY = event.mouseButton.y / CELL_SIZE;
+
+                                            if (mouseX >= 0 && mouseX < FIELD_WIDTH && mouseY >= 0 &&
+                                                mouseY < FIELD_HEIGHT) {
+                                                if (field[mouseX][mouseY].state == CellState::Hidden) {
+                                                    if (field[mouseX][mouseY].hasMine) {
+                                                        gameOver = true;
+                                                        std::cout << "You lost!" << std::endl;
+                                                    } else {
+                                                        field[mouseX][mouseY].state = CellState::Revealed;
+                                                        std::cout << "Opened!" << std::endl;
+                                                    }
+                                                }
+                                            }
+                                        }
+
+                                        // Проверка на победу
+                                        bool allNonMinesRevealed = true;
+                                        for (int x = 0; x < FIELD_WIDTH; x++) {
+                                            for (int y = 0; y < FIELD_HEIGHT; y++) {
+                                                if (!field[x][y].hasMine &&
+                                                    field[x][y].state != CellState::Revealed) {
+                                                    allNonMinesRevealed = false;
+                                                    break;
+                                                }
+                                            }
+                                            if (!allNonMinesRevealed) {
+                                                break;
+                                            }
+                                        }
+
+                                        if (allNonMinesRevealed) {
+                                            win = true;
+                                            std::cout << "You win!" << std::endl;
+                                        }
+                                    }
+                                }
+                            }
+
+                            window.clear();
+
+                            for (int x = 0; x < FIELD_WIDTH; x++) {
+                                for (int y = 0; y < FIELD_HEIGHT; y++) {
+                                    int posX = x * CELL_SIZE;
+                                    int posY = y * CELL_SIZE;
+
+                                    if (field[x][y].state == CellState::Hidden) {
+                                        hiddenSprite.setPosition(posX, posY);
+                                        window.draw(hiddenSprite);
+                                    } else if (field[x][y].state == CellState::Revealed) {
+                                        revealedSprite.setPosition(posX, posY);
+                                        window.draw(revealedSprite);
+                                    } else if (field[x][y].state == CellState::Mine) {
+                                        mineSprite.setPosition(posX, posY);
+                                        window.draw(mineSprite);
+                                    }
+                                }
+                            }
+
+                            if (gameOver) {
+                                sf::RectangleShape overlay(sf::Vector2f(WINDOW_WIDTH, WINDOW_HEIGHT));
+                                overlay.setFillColor(sf::Color(0, 0, 0, 128));
+                                window.draw(overlay);
+                                for (int x = 0; x < FIELD_WIDTH; x++) {
+                                    for (int y = 0; y < FIELD_HEIGHT; y++) {
+                                        int posX = x * CELL_SIZE;
+                                        int posY = y * CELL_SIZE;
+                                        if (field[x][y].hasMine) {
+                                            explodedMineSprite.setPosition(posX, posY);
+                                            window.draw(explodedMineSprite);
+                                        }
+                                    }
+                                }
+                                sf::Text message;
+                                message.setFont(font);
+                                message.setCharacterSize(COMPLEXITY / 40 * 3);
+                                message.setFillColor(sf::Color::White);
+                                message.setString("YOU LOST!");
+                                message.setPosition(
+                                        (WINDOW_WIDTH / 2) - (message.getLocalBounds().width / 2),
+                                        (WINDOW_HEIGHT / 2) - (message.getLocalBounds().height / 2));
+                                window.draw(message);
+                                window.draw(restartText);
+                            }
+
+                            if (win) {
+                                sf::RectangleShape overlay(sf::Vector2f(WINDOW_WIDTH, WINDOW_HEIGHT));
+                                overlay.setFillColor(sf::Color(0, 0, 0, 128));
+                                window.draw(overlay);
+                                for (int x = 0; x < FIELD_WIDTH; x++) {
+                                    for (int y = 0; y < FIELD_HEIGHT; y++) {
+                                        int posX = x * CELL_SIZE;
+                                        int posY = y * CELL_SIZE;
+                                        if (field[x][y].hasMine) {
+                                            mineSprite.setPosition(posX, posY);
+                                            window.draw(mineSprite);
+                                        }
+                                    }
+                                }
+                                sf::Text message;
+                                message.setFont(font);
+                                message.setCharacterSize(COMPLEXITY / 40 * 3);
+                                message.setFillColor(sf::Color::White);
+                                message.setString("YOU WIN!");
+                                message.setPosition(
+                                        (WINDOW_WIDTH / 2) - (message.getLocalBounds().width / 2),
+                                        (WINDOW_HEIGHT / 2) - (message.getLocalBounds().height / 2));
+                                window.draw(message);
+                                window.draw(restartText);
+                            }
+
+                            window.display();
+
+                            if (gameOver || win) {
+                                restartRequested = true;
                                 break;
                             }
                         }
-                        if (!allNonMinesRevealed) {
-                            break;
-                        }
-                    }
-
-                    if (allNonMinesRevealed) {
-                        win = true;
-                        std::cout << "You win!" << std::endl;
                     }
                 }
             }
         }
-
-        window.clear();
-
-        for (int x = 0; x < FIELD_WIDTH; x++) {
-            for (int y = 0; y < FIELD_HEIGHT; y++) {
-                int posX = x * CELL_SIZE;
-                int posY = y * CELL_SIZE;
-
-                if (field[x][y].state == CellState::Hidden) {
-                    hiddenSprite.setPosition(posX, posY);
-                    window.draw(hiddenSprite);
-                } else if (field[x][y].state == CellState::Revealed) {
-                    revealedSprite.setPosition(posX, posY);
-                    window.draw(revealedSprite);
-                } else if (field[x][y].state == CellState::Mine) {
-                    mineSprite.setPosition(posX, posY);
-                    window.draw(mineSprite);
-
-                }
-            }
-        }
-        if (gameOver) {
-            sf::RectangleShape overlay(sf::Vector2f(WINDOW_WIDTH, WINDOW_HEIGHT));
-            overlay.setFillColor(sf::Color(0, 0, 0, 128));
-            window.draw(overlay);
-            for (int x = 0; x < FIELD_WIDTH; x++) {
-                for (int y = 0; y < FIELD_HEIGHT; y++) {
-                    int posX = x * CELL_SIZE;
-                    int posY = y * CELL_SIZE;
-                    if (field[x][y].hasMine) {
-                        explodedMineSprite.setPosition(posX, posY);
-                        window.draw(explodedMineSprite);
-
-                    }
-                }
-            }
-            sf::Text message;
-            sf::Font font;
-            if (!font.loadFromFile("Pixeled.ttf")) {
-                std::cout << "Error arial.ttf" << std::endl;
-                return 1;
-            }
-            message.setFont(font);
-            message.setCharacterSize(COMPLEXITY/40*3);
-            message.setFillColor(sf::Color::White);
-            message.setString("YOU LOST!");
-            message.setPosition((COMPLEXITY/2)-(COMPLEXITY/40*12), COMPLEXITY/2-30);
-            window.draw(message);
-        }
-        if (win) {
-            sf::RectangleShape overlay(sf::Vector2f(WINDOW_WIDTH, WINDOW_HEIGHT));
-            overlay.setFillColor(sf::Color(0, 0, 0, 128));
-            window.draw(overlay);
-            for (int x = 0; x < FIELD_WIDTH; x++) {
-                for (int y = 0; y < FIELD_HEIGHT; y++) {
-                    int posX = x * CELL_SIZE;
-                    int posY = y * CELL_SIZE;
-                    if (field[x][y].hasMine) {
-                        mineSprite.setPosition(posX, posY);
-                        window.draw(mineSprite);
-
-                    }
-                }
-            }
-            sf::Text message;
-            sf::Font font;
-            if (!font.loadFromFile("Pixeled.ttf")) {
-                std::cout << "Error arial.ttf" << std::endl;
-                return 1;
-            }
-            message.setFont(font);
-            message.setCharacterSize(COMPLEXITY/40*3);
-            message.setFillColor(sf::Color::White);
-            message.setString("YOU WIN!");
-            message.setPosition((COMPLEXITY/2)-(COMPLEXITY/40*10), COMPLEXITY/2-30);
-            window.draw(message);
-        }
-
-        window.display();
     }
-
     return 0;
 }
